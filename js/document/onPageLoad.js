d9cce35b03a556ae81b5b0a19ef5e970fa2e7bfe
shahabi110 <shahabi110@gmail.com>
2022-02-05 07:15:21 +0330

    delet js file loader

--- js/document/onPageLoad.js
+++ js/document/onPageLoad.js
@@ -5,9 +5,31 @@ onPageLoads()
 
 function onPageLoads() {
 
+
+
+
       setTimeout(() => {
             StartContract();
             setHeaderWidth();
 
       }, 2000)
 }
+
+
+
+
+
+document.onreadystatechange = function () {
+      if (document.readyState !== "complete") {
+            document.querySelector(
+                  "body").style.visibility = "hidden";
+            document.querySelector(
+                  "#loader").style.visibility = "visible";
+      } else {
+            document.querySelector(
+                  "#loader").style.display = "none";
+            document.querySelector(
+                  "body").style.visibility = "visible";
+            console.log('ready');
+      }
+};
