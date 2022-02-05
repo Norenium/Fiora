cd34b556f039f19facf9ae2522e8b802e10f103e
shahabi110 <shahabi110@gmail.com>
2022-02-05 09:40:52 +0330

    change loader

--- assets/js/document/onPageLoad.js
+++ assets/js/document/onPageLoad.js
@@ -24,10 +24,10 @@ document.onreadystatechange = function () {
             document.querySelector(
                   "body").style.visibility = "hidden";
             document.querySelector(
-                  "#loader").style.visibility = "visible";
+                  "#loader-pad").style.visibility = "visible";
       } else {
             document.querySelector(
-                  "#loader").style.display = "none";
+                  "#loader-pad").style.display = "none";
             document.querySelector(
                   "body").style.visibility = "visible";
             console.log('ready');
