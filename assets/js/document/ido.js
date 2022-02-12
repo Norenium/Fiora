4accd6b9b3f904afeea7094d44c0bca61c1ccab5
shahabi110 <shahabi110@gmail.com>
2022-02-12 19:05:17 +0330

    change dashboard background

--- assets/js/document/ido.js
+++ assets/js/document/ido.js
@@ -3,6 +3,7 @@ function copyToClipBoardCA(){
       console.log('copy click');
       // clipboardData.setData("text/plain", td.textContent);'
       navigator.clipboard.writeText(contractAddress);
+      window.alert('Address has been copied.');
 }
 
 
